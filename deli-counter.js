4d109d1cf48c717f7818178598218a5f1823c607
gilbert <eleazarbiboum@gmail.com>
2022-04-16 14:41:44 -0400

    Assignment Completed

--- deli-counter.js
+++ deli-counter.js
@@ -1,50 +1,71 @@
 // 1. Write your functions here
 //=> "The line is currently: 1. Ada 2. Grace 3. Kent"
-const katzDeli = ["Ada", "Grace", "kent"];
-
-function line(katzDeli){
-  debugger;
-if(katzDeli.count === 0 ){
-console.log("The line is currently empty.")
-}
-else{
-line = "The line is currently:" 
-function rollCall(element, index){
-  katzDeli.forEach((element, index) => rollCall(element, index));
-  console.log(line = `${index + 1}. ${element}`)
-};
-  return line
-}
+function line(array){
+  if (array.length === 0 ){
+    console.log('The line is currently empty!')
+  }
+  else{
+    let emptyarray='';
+    for(let i=0; i<array.length; i++){
+        emptyarray +=  `${i+1}.${array[i]} `
+      }
+         console.log(`The current line is currently: ${emptyarray}`)
+    }  
+  }
   
-}
 // 2. Example Usage
 
-// const katzDeli = []
 
 // takeANumber(katzDeli, "Ada") //=> Welcome, Ada. You are number 1 in line.
 // takeANumber(katzDeli, "Grace") //=> Welcome, Grace. You are number 2 in line.
 // takeANumber(katzDeli, "Kent") //=> Welcome, Kent. You are number 3 in line.
-function takeANumber(katzDeli, person){
-    katzDeli.push(person)
+
+/*
+//old 
+function takeANumber(array, string){
+    array.push(string)
     greeting = []
-  katzDeli.forEach((Name, position) => {
+  array.forEach((Name, position) => {
       greeting = `welcome, ${Name}, you are number ${position +1} in line.`
       console.log(greeting);
   });
-}
+}*/
 
-nowServing 
-// line(katzDeli) //=> "The line is currently: 1. Ada 2. Grace 3. Kent"
-function nowServing(katzDeli){
-    if (katzDeli.count === 0){
-        console.log("Ther is nobody  waiting to be serve!")
-    }
-    else{
-        firstPerson = katzDeli.shift()
-        console.log(`Curently serving ${firstPerson}`)
-    }
+// const katzDeli = []
+function takeANumber(array, Name){
+    array.push(Name)
+  console.log(`welcome, ${Name}, you are number ${array.length} in line.`)
+      
 }
-// nowServing(katzDeli) //=> "Currently serving Ada."
+// takeANumber(katzDeli, "Ada");
+// takeANumber(katzDeli, "Grace");
+// takeANumber(katzDeli, "kent");
+
+
+// nowServing 
+//old function
+/*function nowServing(katzDeli){
+  if (katzDeli.count === 0){
+      console.log("Ther is nobody  waiting to be serve!")
+  }
+  else{
+      firstPerson = katzDeli.shift()
+      console.log(`Curently serving ${firstPerson}`)
+  }
+}
+// line(katzDeli) //=> "The line is currently: 1. Ada 2. Grace 3. Kent"
+nowServing(katzDeli); 
+line(katzDeli); 
+nowServing(katzDeli);  
+line(katzDeli);  
+nowServing(katzDeli);  
+line(katzDeli);
+nowServing(katzDeli)*/ 
+
+function nowServing(array){
+  console.log(`Curently serving ${array.shift()}`);
+}
+//=> "Currently serving Ada."
 
 // line(katzDeli) //=> "The line is currently: 1. Grace 2. Kent"
 
